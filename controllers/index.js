f523ee368b4c6e06a95a129cb275d90f1c405371
Lanay Brown <lanayb.03@gmail.com>
2022-02-19 22:25:56 -0500

    get data to show on page

--- controllers/index.js
+++ controllers/index.js
@@ -5,10 +5,15 @@ const { User, Product} = require('../models');
 router.get('/', async(req, res) => {
     try {
         const userData = await User.findAll();
-       return res.status(200).json(userData);
+        res.status(200).json(userData);
+        const serializedUser = userData.map
+        (user => user.get({plain: true}));
+        console.log(serializedUser);
+        res.render('ayaya');
     } catch(err) {
         return res.status(500).json(err);
     }
+    
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-// require express, sequelize
+// require express, sequelize, path, routes, handlebars
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 // const models = require('./models');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 // set up port number
 const PORT = process.env.PORT || 3001;
 
+// handlebars
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// used for routes
 app.use(routes);
 
 // make sure sequelize is connected first and then listen to the app
