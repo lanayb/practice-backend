677045dece40283f1755afdabd687b6a31226475
Lanay Brown <lanayb.03@gmail.com>
2022-02-20 11:27:58 -0500

    updating

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 // handlebars
 const hbs = exphbs.create({});
 
-app.engine('handlebars', hbs.engine);
+napp.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
